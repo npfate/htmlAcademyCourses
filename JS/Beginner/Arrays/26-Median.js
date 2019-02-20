d7c3bdfbde904a03bc327a8cb0195298b3a7d45e
Chernyshov Alexander <blackraphe@yandex.ru>
2019-02-20 20:12:09 +0300

    Created a script for median calculation

---
+++ JS/Beginner/Arrays/26-Median.js
@@ -0,0 +1,13 @@
+var usersByDay = [1, 2, 3, 4, 5, 6];
+console.log(usersByDay);
+
+if (usersByDay.length % 2 !== 0) {
+  var medianIndex = (usersByDay.length - 1) / 2;
+  var median = usersByDay[medianIndex];
+  console.log(median);
+} else {
+  var leftIndex = usersByDay.length / 2 - 1;
+  var rightIndex = usersByDay.length / 2;
+  var median = (usersByDay[leftIndex] + usersByDay[rightIndex]) / 2
+  console.log(median);
+}
