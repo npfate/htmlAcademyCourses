cdf8ba3a3f830d2aa0072f89be933325837bdb79
Chernyshov Alexander <blackraphe@yandex.ru>
2019-02-21 17:50:40 +0300

    Update 9-arraysIndexOf.js

--- JS/Beginner/Arrays/Challenges/9-arraysIndexOf.js
+++ JS/Beginner/Arrays/Challenges/9-arraysIndexOf.js
@@ -5,9 +5,11 @@ var trainingTime = 30;
 if (trainingTime <= 30) {
   levels[indicators.indexOf('скорость')] += 3;
   levels[indicators.indexOf('ловкость')] += 3;
-} else if (trainingTime > 30 && trainingTime <= 60) {
+}
+if (trainingTime > 30 && trainingTime <= 60) {
   levels[indicators.indexOf('гибкость')] += 3;
-} else {
+}
+if (trainingTime > 60) {
   levels[indicators.indexOf('сила')] += 2;
   levels[indicators.indexOf('выносливость')] += 2;
 } 
