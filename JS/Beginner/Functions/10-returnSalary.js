92514440a27842a6ead4e6f99bbfbefaf5853be7
Chernyshov Alexander <blackraphe@yandex.ru>
2019-02-24 13:09:56 +0300

    Update 10-returnSalary.js

--- JS/Beginner/Functions/10-returnSalary.js
+++ JS/Beginner/Functions/10-returnSalary.js
@@ -1,15 +1,18 @@
-var calculateSalary = function (salary) {
-  var bigSalary = 100000;
-  var dirtySalary = (salary >= bigSalary) ? Math.round(salary * 0.55) : Math.round(salary * 0.65);
-  
-  // var dirtySalary = Math.round(salary * 0.65);
-  // if (salary >= bigSalary) {
-  //   dirtySalary = Math.round(salary * 0.55);
-  // }
-  
-  return dirtySalary;
+var calculateSalary = function (salary) {  
+  var salary = (salary >= 100000) ? Math.round(salary * 0.55) : Math.round(salary * 0.65); 
+  return salary;
 };
 
+// var calculateSalary = function (salary) {
+//   var bigSalary = 100000;  
+//   var dirtySalary = Math.round(salary * 0.65);
+//   if (salary >= bigSalary) {
+//     dirtySalary = Math.round(salary * 0.55);
+//   }
+//   return salary;
+// };
+
+console.log(calculateSalary(50000));
 
 /* Техническое задание
 
