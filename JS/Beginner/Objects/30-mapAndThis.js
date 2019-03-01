fee2b4d79b9c4044a61daf2fc88c443ad6889221
Chernyshov Alexander <blackraphe@yandex.ru>
2019-03-01 22:51:59 +0300

    Create 30-mapAndThis.js

---
+++ JS/Beginner/Objects/30-mapAndThis.js
@@ -0,0 +1,35 @@
+var materialPrice = {
+  'wood': 1000,
+  'stone': 1500,
+  'brick': 2000
+};
+
+var house = {
+  rooms: 5,
+  floors: 3,
+  material: 'wood',
+  coefficient: 9,
+  calculateSquare: function() {
+    let houseSquare = this.rooms * this.coefficient * this.floors;
+    return houseSquare;
+  },
+  calculatePrice: function(name) {
+    let priceOverall = this.calculateSquare() * materialPrice[this.material];
+    return priceOverall;
+  }
+};
+
+
+/* Техническое задание
+
+Мяу! Допиши за меня конфигуратор. Я создал объект house и задал ему несколько свойств: rooms (количество комнат), floors (этажи), material (материал для стен), coefficient (средняя площадь каждой комнаты).
+
+Ещё я завёл мапу materialPrice, в которой записал стоимость каждого возможного материала для строительства.
+
+Добавь в объект два метода: calculateSquare, который будет возвращать площадь дома, и calculatePrice, который будет возвращать стоимость строительства.
+
+Площадь считай так: умножь количество комнат на коэффициент и число этажей в доме.
+
+Цена строительства — произведение площади и стоимости материала дома.
+
+*/
